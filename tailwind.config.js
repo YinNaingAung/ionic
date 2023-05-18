80aaf816b768ddaa34594772d93c31249fbdf818
YinNaingAung <yinnaingaung10@gmail.com>
2023-05-19 00:30:03 +0630

    41th

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   darkMode: 'class',
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow:{
+        'position':'0 10px 60px -15px rgba(10,10,0)'
+      }
+    },
   },
   plugins: [],
 }
